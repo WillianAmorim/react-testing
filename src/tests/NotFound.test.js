20a36934a970644df112c7b4ad42065abd7e390a
WillianAmorim <willian.amorim1@outlook.com>
2021-11-23 02:30:54 -0300

    feito requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /page requested not found/i });
+    const imageError = screen.getByAltText(/pikachu crying/i);
+    expect(h2).toBeInTheDocument();
+    expect(imageError).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
